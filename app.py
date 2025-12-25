ef695d697dc8975ba726d967616b473d55b2e3f9
mauriciok10lg-wq <mauriciok10lg@gmail.com>
2025-12-25 10:51:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+rom flask import Flask, render_template, redirect, url_for, request, session
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 import json
